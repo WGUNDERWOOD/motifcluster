a0d2497b3fb87da677115fdfc7da59c01941e1b4
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2020-03-30 16:09:59 -0500

    adding more docs

--- motifclustr/R/spectral.R
+++ motifclustr/R/spectral.R
@@ -1,103 +1,177 @@
-#' Compute first few eigenvalues and eigenvectors.
+#' Compute first few eigenvalues and eigenvectors
 #'
 #' Compute the first few eigenvalues (by magnitude) and
 #' associated eigenvectors of a matrix.
-#' @param mat The symmetric matrix for which eigenvalues and eigenvectors are to be calculated.
-#' @param l The number of eigenvalues and eigenvectors to calculate.
-#' @return The first \code{l} eigenvalues (by magnitude) and associated eigenvectors of \code{mat}.
-#' @return A list with two entries: \code{vals} contains the a vector of the first few eigenvalues,
-#' and \code{vects} contains a matrix of the associated eigenvectors.
+#' @param mat Symmetric matrix for which eigenvalues and eigenvectors are to be calculated.
+#' @param num_eigs Number of eigenvalues and eigenvectors to calculate.
+#' @return A list with two entries: \code{vals} contains the a length \code{num_eigs} vector
+#' of the first few eigenvalues,
+#' and \code{vects} contains a \code{nrow(mat)} by \code{num_eigs} matrix
+#' of the associated eigenvectors.
 #' @keywords eigenvalue eigenvector spectrum matrix
 #' @export
 #' @examples
-#' get_first_eigs(matrix(rep(1,9), nrow=3), 2)
+#' get_first_eigs(matrix(rep(1,9), nrow=3), num_eigs=2)
 
-get_first_eigs <- function(mat, l){
+get_first_eigs <- function(mat, num_eigs){
 
   # check args
-  if(!all.equal(l, as.integer(l))){
-    stop("l must be an integer.")
-  }
-  if(!(l > 0)){
-    stop("l must be at least 1.")
-  }
   if(!is.matrix(mat)){
     stop("mat must be a matrix.")}
   if(!isSymmetric(mat)){
     stop("mat must be symmetric.")
   }
+  if(!all.equal(num_eigs, as.integer(num_eigs))){
+    stop("num_eigs must be an integer.")
+  }
+  if(!(num_eigs > 0)){
+    stop("num_eigs must be at least 1.")
+  }
 
   # get spectrum
-  ans_eigs <- eigs(mat, l, which = 'SM')
+  ans_eigs <- eigs(mat, num_eigs, which = "SM")
 
   # order eigenvalues and eigenvectors
-  inds <- seq(l, 1, -1)
-  vects <- Re(ans_eigs[['vectors']])[,inds]
-  vals <- Re(ans_eigs[['values']])[inds]
+  inds <- seq(num_eigs, 1, -1)
+  vects <- Re(ans_eigs[["vectors"]])[,inds]
+  vals <- Re(ans_eigs[["values"]])[inds]
 
   # return a list
   ans_spect <- list()
-  ans_spect[['vects']] = vects
-  ans_spect[['vals']]  = vals
+  ans_spect[["vects"]] = vects
+  ans_spect[["vals"]]  = vals
 
   return(ans_spect)
 }
 
-buildLaplacian = function(G, typeLap=c('comb','rw')){
+#' Build a Laplacian matrix
+#'
+#' Build a Laplacian matrix (combinatorial Laplacian or random-walk Laplacian)
+#' from a symmetric (weighted) graph adjacency matrix.
+#' @param adj_mat Symmetric adjacency matrix from which to build the Laplacian.
+#' @param type_lap Type of Laplacian to build. One of \code{"comb"} or \code{"rw"}.
+#' @return The specified Laplacian matrix.
+#' @keywords laplacian matrix
+#' @export
+#' @examples
+#' build_laplacian(matrix(rep(1,9), nrow=3), type_lap="rw")
 
-  # Builds various types of Laplacian matrix,
-  # given an adjacency matrix G and a type of Laplacian.
+build_laplacian <- function(adj_mat, type_lap=c("comb","rw")){
 
-  # typeLap
-  # 'comb'      combinatorial Laplacian                           L = D - G
-  # 'rw'        random walk Laplacian (row-normalised)            L = D^(-1) (D - G)
-
-  typeLap = match.arg(typeLap)
-
-  degsG = apply(G, 1, sum)
-  n = nrow(G)
-
-  if (typeLap=='comb'){
-    D = diag(degsG)
-    L =  D - G
+  # check args
+  if(!is.matrix(adj_mat)){
+    stop("adj_mat must be a matrix.")}
+  if(!isSymmetric(adj_mat)){
+    stop("adj_mat must be symmetric.")
   }
-  else if (typeLap == 'rw'){
-    Dinv = diag(degsG^(-1))
-    L =  diag(n) - Dinv %*% G
+  type_lap <- match.arg(type_lap)
+
+  # initialize parameters
+  degs_adj_mat <- apply(adj_mat, 1, sum)
+  n <- nrow(adj_mat)
+
+  # combinatorial Laplacian
+  if (type_lap=='comb'){
+    degs_matrix <- diag(degsadj_mat)
+    L <-  degs_matrix - adj_mat
+  }
+
+  # random-walk Laplacian
+  else if (type_lap == 'rw'){
+    inv_degs_matrix <- diag(degs_adj_mat^(-1))
+    L <-  diag(n) - inv_degs_matrix %*% adj_mat
   }
 
   return(L)
 }
 
-runLapEmb = function(G, topk, typeLap=c('comb','rw')){
+#' Run Laplace embedding
+#'
+#' Run Laplace embedding on a symmetric (weighted) adjacency matrix
+#' with a specified number of eigenvalues and eigenvectors.
+#' @param adj_mat Symmetric adjacency matrix to be embedded.
+#' @param num_eigs Number of eigenvalues and eigenvectors for the embedding.
+#' @param type_lap Type of Laplacian for the embedding. One of \code{"comb"} or \code{"rw"}.
+#' @return A list with two entries: \code{vals} contains the a length \code{num_eigs} vector
+#' of the first few eigenvalues of the Laplacian,
+#' and \code{vects} contains a \code{nrow(mat)} by \code{num_eigs} matrix
+#' of the associated eigenvectors.
+#' @keywords laplacian embedding eigenvalue eigenvector matrix
+#' @export
+#' @examples
+#' run_laplace_embedding(matrix(rep(1,9), nrow=3), num_eigs=2, type_lap="rw")
 
-  # Runs Laplace embedding of an adjacency matrix G, given number of clusters and
-  # Laplacian type. Returns a list with $vects and $vals.
+run_laplace_embedding <- function(adj_mat, num_eigs, type_lap=c("comb","rw")){
 
-  typeLap = match.arg(typeLap)
+  # check args
+  if(!is.matrix(adj_mat)){
+    stop("adj_mat must be a matrix.")}
+  if(!isSymmetric(adj_mat)){
+    stop("adj_mat must be symmetric.")
+  }
+  if(!all.equal(num_eigs, as.integer(num_eigs))){
+    stop("num_eigs must be an integer.")
+  }
+  if(!(num_eigs > 0)){
+    stop("num_eigs must be at least 1.")
+  }
+  type_lap <- match.arg(type_lap)
 
-  L = buildLaplacian(G, typeLap)
-  ansSpect = computeTopSpectrum(L, typeLap, topk)
+  # build Laplacian
+  laplacian <- build_laplacian(adj_mat, type_lap)
+  ans_spect <- get_first_eigs(laplacian, type_lap, num_eigs)
 
-  return(ansSpect)
-
+  return(ans_spect)
 }
 
-runMotifEmb = function(G, motif_name, type, typeLap, numEigs){
+#' Run motif embedding
+#'
+#' Calculate a motif adjacency matrix for a given motif and motif type,
+#' restrict it to its largest connected component,
+#' and then run Laplace embedding with specified Laplacian type and
+#' number of eigenvalues and eigenvectors.
+#' @param adj_mat Symmetric adjacency matrix to be embedded.
+#' @param motif_name Motif used for the motif adjacency matrix.
+#' @param motif_type Type of motif adjacency matrix to use.
+#' One of \code{"func"} or \code{"struc"}.
+#' @param type_lap Type of Laplacian for the embedding. One of \code{"comb"} or \code{"rw"}.
+#' @param num_eigs Number of eigenvalues and eigenvectors for the embedding.
+#' @return A list with 7 entries:
+#' \code{adj_mat}, the original adjacency matrix;
+#' \code{motif_adj_mat}, the motif adjacency matrix;
+#' \code{comps}, the indices of the largest connected component of the motif adjacency matrix;
+#' \code{adj_mat_comps}, the original adjacency matrix restricted to the largest connected
+#' component of the motif adjacency matrix;
+#' \code{motif_adj_mat_comps}, the motif adjacency matrix restricted to its
+#' largest connected component;
+#' \code{vals}, the eigenvalues associated with the Laplace embedding
+#' of the motif adjacency matrix;
+#' \code{vects}, the eigenvectors associated with the Laplace embedding
+#' of the motif adjacency matrix;
+#' @keywords motif adjacency matrix laplacian embedding
 
-  # Runs motif-based embedding on a given adjacency matrix.
+run_motif_embedding <- function(adj_mat, motif_name, motif_type, num_eigs, type_lap){
 
-  M = motifAdjacency(G, motif_name, type)
-  comps = largestComponent(M)
-  M = M[comps,comps, drop=FALSE]
-  Gcomps = G[comps,comps, drop=FALSE]
-  spect = runLapEmb(M,numEigs,typeLap)
-  ans = list()
-  ans$vects = spect$vects
-  ans$vals = spect$vals
-  ans$comps = comps
-  ans$M = M
-  ans$Gcomps = Gcomps
+  # build motif adjacency matrix
+  motif_adj_mat <- build_motif_adjacency_matrix(adj_mat, motif_name, motif_type)
+
+  # restrict to largest connected component
+  comps <- get_largest_component(motif_adj_mat)
+  adj_mat_comps <- adj_mat[comps, comps, drop=FALSE]
+  motif_adj_mat_comps <- motif_adj_mat[comps, comps, drop=FALSE]
+
+  # Laplace embedding
+  spect <- run_laplace_embedding(motif_adj_mat, num_eigs, type_lap)
+
+  # return list
+  ans <- list()
+  ans$adj_mat <- adj_mat
+  ans$motif_adj_mat <- motif_adj_mat
+  ans$comps <- comps
+  ans$adj_mat_comps <- adj_mat_comps
+  ans$motif_adj_mat_comps <- motif_adj_mat_comps
+  ans$vals <- spect$vals
+  ans$vects <- spect$vects
 
   return(ans)
 }
