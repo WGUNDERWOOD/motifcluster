917a8f6c2c3a1f141009a8c679e3c39f4fe61794
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2020-04-21 12:33:09 -0500

    added get largest component to python

--- python/motifcluster/utils.py
+++ python/motifcluster/utils.py
@@ -1,23 +1,43 @@
 import numpy as np
-from scipy.sparse import csr_matrix
-from scipy.sparse import diags
+import networkx as nx
+from scipy import sparse
 
 def a_b_one(a, b):
 
-  a_sparse = csr_matrix(a)
-  b_sparse = csr_matrix(b)
+  a_sparse = sparse.csr_matrix(a)
+  b_sparse = sparse.csr_matrix(b)
   n = a.shape[0]
   ones_vec = np.ones(n)
   ans = (a_sparse.T.multiply(b_sparse @ ones_vec)).T
 
-  return csr_matrix(ans)
+  return sparse.csr_matrix(ans)
+
 
 def a_one_b(a, b):
 
-  a_sparse = csr_matrix(a)
-  b_sparse = csr_matrix(b)
+  a_sparse = sparse.csr_matrix(a)
+  b_sparse = sparse.csr_matrix(b)
   n = a.shape[0]
   ones_vec = np.ones(n)
   ans = (a_sparse.multiply(ones_vec @ b))
 
-  return csr_matrix(ans)
+  return sparse.csr_matrix(ans)
+
+
+def drop0_killdiag(some_mat):
+
+  ans = sparse.csr_matrix(some_mat)
+  I = sparse.identity(ans.shape[0])
+  ans = ans - I.multiply(ans)
+
+  return(sparse.csr_matrix(ans))
+
+
+def get_largest_component(adj_mat):
+
+  adj_mat_sparse = sparse.csr_matrix(adj_mat)
+  gr = nx.from_scipy_sparse_matrix(adj_mat_sparse > 0)
+  verts_to_keep = max(nx.connected_components(gr), key=len)
+  verts_to_keep = sorted(verts_to_keep)
+
+  return verts_to_keep
--- python/tests/test_utils.py
+++ python/tests/test_utils.py
@@ -3,7 +3,7 @@ from motifcluster import utils as mcut
 
 # import other dependencies
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy import sparse
 
 # tests
 
@@ -11,20 +11,19 @@ def test_a_b_one():
 
   a_dense = np.array(range(-4, 5)).reshape((3, 3))
   b_dense = np.array(range(-1, 8)).reshape((3, 3))
-  a_sparse = csr_matrix(a_dense)
-  b_sparse = csr_matrix(b_dense)
+  a_sparse = sparse.csr_matrix(a_dense)
+  b_sparse = sparse.csr_matrix(b_dense)
 
   ans = np.array([0, 0, 0, -9, 0, 9, 36, 54, 72]).reshape((3, 3))
-  ans = csr_matrix(ans)
 
   assert np.allclose(
     mcut.a_b_one(a_dense, b_dense).toarray(),
-    ans.toarray()
+    ans
   )
 
   assert np.allclose(
     mcut.a_b_one(a_sparse, b_sparse).toarray(),
-    ans.toarray()
+    ans
   )
 
 
@@ -32,18 +31,52 @@ def test_a_one_b():
 
   a_dense = np.array(range(-4, 5)).reshape((3, 3))
   b_dense = np.array(range(-1, 8)).reshape((3, 3))
-  a_sparse = csr_matrix(a_dense)
-  b_sparse = csr_matrix(b_dense)
+  a_sparse = sparse.csr_matrix(a_dense)
+  b_sparse = sparse.csr_matrix(b_dense)
 
   ans = np.array([-24, -27, -24, -6, 0, 12, 12, 27, 48]).reshape((3, 3))
-  ans = csr_matrix(ans)
 
   assert np.allclose(
     mcut.a_one_b(a_dense, b_dense).toarray(),
-    ans.toarray()
+    ans
   )
 
   assert np.allclose(
     mcut.a_one_b(a_sparse, b_sparse).toarray(),
-    ans.toarray()
+    ans
   )
+
+
+def test_drop0_kill_diag():
+
+  adj_mat_dense = np.array(range(-1, 8)).reshape((3,3))
+  adj_mat_sparse = sparse.csr_matrix(adj_mat_dense)
+
+  ans = np.array([0, 0, 1, 2, 0, 4, 5, 6, 0]).reshape((3,3))
+
+  assert np.allclose(
+    mcut.drop0_killdiag(adj_mat_dense).toarray(),
+    ans
+  )
+
+  assert np.allclose(
+    mcut.drop0_killdiag(adj_mat_sparse).toarray(),
+    ans
+  )
+
+
+def test_get_largest_component():
+
+  adj_mat_dense = np.array([
+    0, 0, 0, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 0, 0, 2,
+    3, 0, 0, 0, 0,
+    0, 4, 0, 0, 0
+  ]).reshape((5, 5))
+
+  adj_mat_sparse = sparse.csr_matrix(adj_mat_dense)
+  ans = [1, 2, 4]
+
+  assert mcut.get_largest_component(adj_mat_dense) == ans
+  assert mcut.get_largest_component(adj_mat_sparse) == ans
