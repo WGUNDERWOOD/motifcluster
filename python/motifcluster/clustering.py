28bc4fa35c3268c1f2866abbd9ca336a283bd61a
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2020-04-24 19:32:18 -0500

    wrote docs for spectral py

--- python/motifcluster/clustering.py
+++ python/motifcluster/clustering.py
@@ -1,5 +1,9 @@
-from sklearn.cluster import KMeans
+"""
+Functions for spectral clustering
+are in `motifcluster.clustering`.
+"""
 
+from sklearn.cluster import KMeans
 
 def cluster_spectrum(spectrum, num_clusts):
 
@@ -31,62 +35,82 @@ def cluster_spectrum(spectrum, num_clusts):
   return(cluster_assigns)
 
 
-"""
-#' Run motif-based clustering
-#'
-#' Run motif-based clustering on the adjacency matrix of a
-#' (weighted directed) network,
-#' using a specified motif, motif type, weighting scheme,
-#' embedding dimension, number of clusters and Laplacian type.
-#' @param adj_mat Adjacency matrix to be embedded.
-#' @param motif_name Motif used for the motif adjacency matrix.
-#' @param motif_type Type of motif adjacency matrix to use.
-#' One of \code{"func"} or \code{"struc"}.
-#' @param mam_weight_type Weighting scheme for the motif adjacency matrix.
-#' One of \code{"unweighted"}, \code{"mean"} or \code{"product"}.
-#' @param mam_method The method to use for building the motif adjacency matrix.
-#' One of \code{"sparse"} or \code{"dense"}.
-#' @param num_eigs Number of eigenvalues and eigenvectors for the embedding.
-#' @param type_lap Type of Laplacian for the embedding.
-#' One of \code{"comb"} or \code{"rw"}.
-#' @param num_clusts The number of clusters to find.
-#' @return A list with 8 entries:
-#' \itemize{
-#'   \item \code{adj_mat}: the original adjacency matrix.
-#'   \item \code{motif_adj_mat}: the motif adjacency matrix.
-#'   \item \code{comps}: the indices of the largest connected component
-#'     of the motif adjacency matrix.
-#'   \item \code{adj_mat_comps}: the original adjacency matrix restricted
-#'     to the largest connected component of the motif adjacency matrix.
-#'   \item \code{motif_adj_mat_comps}: the motif adjacency matrix restricted
-#'     to its largest connected component.
-#'   \item \code{vals}: a length-\code{num_eigs} vector containing the
-#'     eigenvalues associated with the Laplace embedding
-#'     of the restricted motif adjacency matrix.
-#'   \item \code{vects}: a matrix
-#'     containing the eigenvectors associated with the Laplace embedding
-#'     of the restricted motif adjacency matrix.
-#'   \item \code{clusts}: a vector containing integers representing the
-#'     cluster assignment of each vertex.
-#' }
-#' @examples
-#' adj_mat = matrix(c(1:9), nrow = 3)
-#' run_motif_clustering(adj_mat, "M1", "func",
-#'   "mean", "sparse", 2, "rw", 2)
-#' @export
+def run_motif_clustering(adj_mat, motif_name,
+  motif_type = "struc",
+  mam_weight_type = "unweighted",
+  mam_method = "sparse",
+  num_eigs = 1,
+  type_lap = "comb",
+  num_clusts = 2):
 
-run_motif_clustering = function(adj_mat, motif_name,
-  motif_type = c("struc", "func"),
-  mam_weight_type = c("unweighted", "mean", "product"),
-  mam_method = c("sparse", "dense"),
-  num_eigs,
-  type_lap = c("comb", "rw"),
-  num_clusts) {
+  """
+  Run motif-based clustering.
 
-  motif_type = match.arg(motif_type)
-  mam_weight_type = match.arg(mam_weight_type)
-  mam_method = match.arg(mam_method)
-  type_lap = match.arg(type_lap)
+  Run motif-based clustering on the adjacency matrix of a
+  (weighted directed) network,
+  using a specified motif, motif type, weighting scheme,
+  embedding dimension, number of clusters and Laplacian type.
+
+  Parameters
+  ----------
+  adj_mat : matrix
+    Adjacency matrix to be embedded.
+  motif_name : str
+    Motif used for the motif adjacency matrix.
+  motif_type : str
+    Type of motif adjacency matrix to use.
+    One of `"func"` or `"struc"`.
+  mam_weight_type : str
+    Weighting scheme for the motif adjacency matrix.
+    One of `"unweighted"`, `"mean"` or `"product"`.
+  mam_method : str
+    The method to use for building the motif adjacency matrix.
+    One of `"sparse"` or `"dense"`.
+  num_eigs : int
+    Number of eigenvalues and eigenvectors for the embedding.
+  type_lap : str
+    Type of Laplacian for the embedding.
+    One of `"comb"` or `"rw"`.
+  num_clusts : int
+    The number of clusters to find.
+
+  Returns
+  -------
+  adj_mat : matrix
+    The original adjacency matrix.
+  motif_adj_mat : matrix
+    The motif adjacency matrix.
+  comps : list
+    The indices of the largest connected component
+    of the motif adjacency matrix.
+  adj_mat_comps : matrix
+    The original adjacency matrix restricted
+    to the largest connected component of the motif adjacency matrix.
+  motif_adj_mat_comps : matrix
+    The motif adjacency matrix restricted
+    to its largest connected component.
+  vals : list
+    A length-`num_eigs` list containing the
+    eigenvalues associated with the Laplace embedding
+    of the restricted motif adjacency matrix.
+  vects : matrix
+    A matrix containing the eigenvectors associated with the Laplace
+    embedding of the restricted motif adjacency matrix.
+  clusts :
+    A vector containing integers representing the
+    cluster assignment of each vertex.
+
+  Examples
+  --------
+  >>> adj_mat = np.array(range(1, 10)).reshape((3, 3))
+  >>> run_motif_clustering(adj_mat, "M1", "func",
+  ...   "mean", "sparse", 2, "rw", 2)
+  """
+
+  assert motif_type in ["struc", "func"]
+  assert mam_weight_type in ["unweighted", "mean", "product"]
+  assert mam_method in ["sparse", "dense"]
+  assert type_lap in ["comb", "rw"]
 
   spectrum = run_motif_embedding(
     adj_mat, motif_name, motif_type, mam_weight_type,
@@ -94,16 +118,15 @@ run_motif_clustering = function(adj_mat, motif_name,
 
   cluster_assigns = cluster_spectrum(spectrum, num_clusts)
 
-  ans = list()
-  ans$adj_mat = adj_mat
-  ans$motif_adj_mat = spectrum$motif_adj_mat
-  ans$comps = spectrum$comps
-  ans$adj_mat_comps = spectrum$adj_mat_comps
-  ans$motif_adj_mat_comps = spectrum$motif_adj_mat_comps
-  ans$vals = spectrum$vals
-  ans$vects = spectrum$vects
-  ans$clusts = cluster_assigns
+  ans = {
+    "adj_mat": adj_mat,
+    "motif_adj_mat": spectrum["motif_adj_mat"],
+    "comps": spectrum["comps"],
+    "adj_mat_comps": spectrum["adj_mat_comps"],
+    "motif_adj_mat_comps": spectrum["motif_adj_mat_comps"],
+    "vals": spectrum["vals"],
+    "vects": spectrum["vects"],
+    "clusts": cluster_assigns
+  }
 
   return(ans)
-}
-"""
