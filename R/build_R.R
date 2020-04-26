79f8768d88e0fac32efa6ec57f34206807bf874f
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2020-04-26 14:07:44 -0500

    adding covr

--- R/build_R.R
+++ R/build_R.R
@@ -3,11 +3,13 @@ Sys.setenv(RSTUDIO_PANDOC="/usr/bin/pandoc")
 library(devtools)
 library(roxygen2)
 library(lintr)
+library(covr)
 
-check()
-build_manual(path = "./doc")
-build_vignettes()
-lint_package(linters = with_defaults(
-               object_name_linter = NULL,
-               object_usage_linter = NULL,
-               cyclocomp_linter = NULL))
+#check()
+#build_manual(path = "./doc")
+#build_vignettes()
+report()
+#lint_package(linters = with_defaults(
+               #object_name_linter = NULL,
+               #object_usage_linter = NULL,
+               #cyclocomp_linter = NULL))
