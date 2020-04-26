d0c37d15d38254c7d36d68570cbccd230e34f542
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2020-04-26 16:18:51 -0500

    updated setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='motifcluster',
     version='0.0.1',
     description='Motif-Based Spectral Clustering of Weighted Directed Networks',
-    license="GPLv3", #TODO licence file?
-    long_description=("Construct motif adjacency matrices for (weighted directed)",
-        "networks, and use them for spectral clustering. Also provides random sampling",
-        "methods for weighted directed networks."), #TODO move this to a md file in python/
+    license="GPLv3",
+    long_description="Construct motif adjacency matrices for (weighted directed) " +
+        "networks, and use them for spectral clustering. Also provides random sampling " +
+        "methods for weighted directed networks.",
     author='William George Underwood',
     author_email='wgu2@princeton.edu',
     packages=['motifcluster'],
