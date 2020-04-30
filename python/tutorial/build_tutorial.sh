13278515e62a24f42e2f2b8b14c6eeac48ef0eb2
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2020-04-29 22:57:26 -0500

    writing py tutorial

--- python/tutorial/build_tutorial.sh
+++ python/tutorial/build_tutorial.sh
@@ -1,4 +1,5 @@
 pdflatex motifcluster_tutorial.tex &&
+    bibtex motifcluster_tutorial &&
     pythontex motifcluster_tutorial.tex &&
     pdflatex motifcluster_tutorial.tex &&
     pdflatex motifcluster_tutorial.tex
