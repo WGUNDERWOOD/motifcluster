a329bc3a25c156d2627dc04a30a36fb412d8a3ea
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2020-05-03 13:42:15 -0500

    updated sticker color

--- sticker/build_sticker.sh
+++ sticker/build_sticker.sh
@@ -1,4 +1,4 @@
 pdflatex sticker.tex &&
     convert -density 500 sticker.pdf sticker.png &&
-    hexsticker sticker.png -o hex_sticker.png --border-size 120 --border-color "#74668A" --supersample 2 &&
+    hexsticker sticker.png -o hex_sticker.png --border-size 120 --border-color "#6a668A" --supersample 2 &&
     optipng -o3 hex_sticker.png
