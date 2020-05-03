77f46731324a1fc18ec5f1f20452347fdd856555
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2020-05-03 12:23:18 -0500

    updating sticker colors

--- sticker/build_sticker.sh
+++ sticker/build_sticker.sh
@@ -1,5 +1,4 @@
 pdflatex sticker.tex &&
-    pdflatex sticker.tex &&
-    pdflatex sticker.tex &&
     convert -density 500 sticker.pdf sticker.png &&
-    hexsticker sticker.png -o hex_sticker.png --border-size 120 --border-color "#4c4a5b" --supersample 2
+    hexsticker sticker.png -o hex_sticker.png --border-size 120 --border-color "#74668A" --supersample 2 &&
+    optipng -o3 hex_sticker.png
