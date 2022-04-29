911767a1183cae4471c2158cc579f8cb1a330b9f
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2022-04-28 21:30:00 -0400

    updating python version number to 0.2.0

--- python/doc/conf.py
+++ python/doc/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, William George Underwood'
 author = 'William George Underwood, Andrew Elliott'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
