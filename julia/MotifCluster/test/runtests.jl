33abc7fae0854cde7199df3292295c4d60452047
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-06-19 20:31:00 -0400

    updating project files julia

--- julia/MotifCluster/test/runtests.jl
+++ julia/MotifCluster/test/runtests.jl
@@ -4,10 +4,20 @@ using Random
 using Distributions
 using SparseArrays
 using LinearAlgebra
+using Aqua
 
-include("test_clustering.jl")
-include("test_indicators.jl")
-include("test_spectral.jl")
-include("test_motifadjacency.jl")
-include("test_utils.jl")
-include("test_sampling.jl")
+# Aqua tests
+Aqua.test_ambiguities(MotifCluster)
+Aqua.test_unbound_args(MotifCluster)
+Aqua.test_undefined_exports(MotifCluster)
+Aqua.test_project_extras(MotifCluster)
+Aqua.test_stale_deps(MotifCluster, ignore=[:Aqua])
+Aqua.test_deps_compat(MotifCluster)
+Aqua.test_project_toml_formatting(MotifCluster)
+
+#include("test_clustering.jl")
+#include("test_indicators.jl")
+#include("test_spectral.jl")
+#include("test_motifadjacency.jl")
+#include("test_utils.jl")
+#include("test_sampling.jl")
