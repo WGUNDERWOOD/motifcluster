7ae5a3191f9b5ee01feed6cdccbe925481b92288
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-01-29 18:54:18 -0500

    updating deps

--- shell.nix
+++ shell.nix
@@ -7,36 +7,39 @@ in pkgs.mkShell {
 
         # python
         python3
+        python3Packages.build
+        python3Packages.matplotlib
+        python3Packages.networkx
+        python3Packages.numpy
         python3Packages.pandas
+        python3Packages.pylint
         python3Packages.pytest
         python3Packages.pytest-cov
-        python3Packages.numpy
-        python3Packages.networkx
-        python3Packages.scipy
         python3Packages.scikit-learn
-        python3Packages.pylint
+        python3Packages.scipy
+        python3Packages.seaborn
         python3Packages.sphinx
         python3Packages.sphinx-rtd-theme
-        python3Packages.build
         python3Packages.twine
 
         # R
         R
         qpdf
-        rPackages.igraph
-        rPackages.devtools
-        rPackages.lintr
         rPackages.covr
+        rPackages.devtools
         rPackages.filesstrings
-        rPackages.RSpectra
+        rPackages.igraph
+        rPackages.lintr
         rPackages.mclust
+        rPackages.RSpectra
 
         # julia
-        # TODO
+        julia
 
        # other
        graphviz
        gprof2dot
+       texlive.combined.scheme-full
        # TODO hexsticker
     ];
 }
